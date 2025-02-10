2cf4fc0cdecf7c44189ed9b75017038465914da7
Luke <luke.manongsong@gmail.com>
2025-02-10 21:28:29 +0800

    fix: mobile responsiveness

--- src/app/LandingPage.jsx
+++ src/app/LandingPage.jsx
@@ -7,7 +7,7 @@ import { motion } from "motion/react";
 
 export default function LandingPage() {
     return (
-        <section className="md:pt-44 pt-20 pb-36 w-fit mx-auto" id="home">
+        <section className="md:pt-44 pt-20 pb-36 mx-auto md:w-fit" id="home">
           <motion.div className="text-center pb-10"
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
@@ -20,7 +20,7 @@ export default function LandingPage() {
               solutions, building full-stack web applications, and working on data engineering projects to ensure data
               quality.
             </p>
-            <div className="lg:flex grid-cols-2 grid justify-center gap-4 py-6 items-center w-fit min-w-[300px] mx-auto">
+            <div className="lg:flex flex flex-wrap gap-4 justify-center w-full mx-auto lg:w-fit py-3">
               <Link href="/contact">
               <motion.button   
               whileHover={{ scale: 1.1 }}
@@ -28,9 +28,11 @@ export default function LandingPage() {
               className="px-4 py-2 text-nowrap bg-gradient-to-r from-cyan-800 to-teal-600 text-white rounded-md flex items-center gap-2 text-sm md:text-base w-fit justify-center">Get in touch <FaArrowRightLong /></motion.button></Link>
               <button className="flex gap-2 items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md text-nowrap cursor-pointer text-sm md:text-base w-fit justify-center">
                 <a href="/CV_Luke_Manongsong.pdf" download className="flex items-center"><IoDownloadOutline className="text-lg mr-1" />Download CV</a></button>
-              <a href="https://www.linkedin.com/in/luke-manongsong-ba5396298/" target="_blank" rel="noopener noreferrer"  className="ml-auto md:ml-0"><motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><AiFillLinkedin className="text-5xl text-teal-900 cursor-pointer"/></motion.div></a>
-              <a href="https://github.com/luke08082000" target="_blank" rel="noopener noreferrer"  className="mr-auto md:mr-0"><motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><AiFillGithub className="text-5xl text-teal-900 cursor-pointer"/></motion.div></a>
-            </div>
+              </div>
+              <div className='flex gap-6 items-center justify-center w-fit mx-auto'>
+                <a href="https://github.com/luke08082000" target="_blank" rel="noopener noreferrer"  className="mr-auto md:mr-0"><motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><AiFillGithub className="text-5xl text-teal-900 cursor-pointer"/></motion.div></a>
+                <a href="https://www.linkedin.com/in/luke-manongsong-ba5396298/" target="_blank" rel="noopener noreferrer"  className="ml-auto md:ml-0"><motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><AiFillLinkedin className="text-5xl text-teal-900 cursor-pointer"/></motion.div></a>
+              </div>
           </motion.div>
         </section>
     )
--- src/app/NavBar.jsx
+++ src/app/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
     menuToggle ? setMenuToggle(false) : setMenuToggle(true);
   }
     return (
-        <nav className="md:fixed md:top-5 md:left-1/2 md:transform md:-translate-x-1/2 lg:w-auto md:px-6 md:py-2 py-4 px-2 bg-teal-500 md:rounded-3xl z-10">
+        <nav className="md:fixed md:top-5 md:left-1/2 md:transform md:-translate-x-1/2 lg:w-auto md:px-6 md:py-2 py-4 px-2 bg-teal-500 md:rounded-xl z-10">
           <AiOutlineMenu className="text-3xl fill-teal-900 md:hidden ml-auto" onClick={handleOnToggle}/>
           <ul className={menuToggle ? 'block px-2 text-gray-100' : 'hidden md:flex justify-center md:gap-6 items-center h-full text-sm md:text-lg text-gray-100'}>
             <li className={`md:hover:text-white`}><a href="#home">Home</a></li>
